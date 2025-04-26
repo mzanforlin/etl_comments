ccc2d8a4aa2df6b347f6fe52c92db59f774ed563
Matheus Zanforlin <zanforlin14@gmail.com>
2025-04-26 14:39:50 -0300

    add Load

--- elt_comments.py
+++ elt_comments.py
@@ -2,13 +2,35 @@ import pandas as pd
 from sqlalchemy import create_engine
 import requests
 import psycopg2
+from dotenv import load_dotenv
 import json
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 
 url = 'https://jsonplaceholder.typicode.com/posts'
 
-response_status = requests.get(url).status_code
-response_json = requests.get(url)
-resp = response_json.json()
-df = pd.DataFrame(resp).json
+def extrair_dados():
 
-display(df)
+    resp = requests.get(url)
+
+    if resp.status_code == 200:
+        dados_json = resp.json()
+        df = pd.DataFrame(dados_json)
+        df_final = print(df)
+        return df_final
+    else:
+       print(f'deu um erro aqui : {resp.status_code()}')
+       return None
+    
